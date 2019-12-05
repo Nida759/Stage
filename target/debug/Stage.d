b0dae798a17eba23f5bb902e94f022e1e376dc89
Your Name <Your Email>
2019-12-05 07:48:31 +0000

    added commit

---
+++ target/debug/Stage.d
@@ -0,0 +1,1 @@
+/home/nida/Stage/Stage/target/debug/Stage: /home/nida/Stage/Stage/src/main.rs
---
+++ target/debug/deps/Stage-02b88d414b7c981c.d
@@ -0,0 +1,5 @@
+/home/nida/Stage/Stage/target/debug/deps/Stage-02b88d414b7c981c: src/main.rs
+
+/home/nida/Stage/Stage/target/debug/deps/Stage-02b88d414b7c981c.d: src/main.rs
+
+src/main.rs:
